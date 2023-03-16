e28d36e8808d1db00d03c9620de67460911cd630
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2023-03-16 15:46:28 -0600

    Update bellCurve.js

--- bellCurve.js
+++ bellCurve.js
@@ -2,8 +2,8 @@ const { random, floor, ceil, max } = Math, width = process.stdout.columns - 10, 
 
 for (let i = 0, modifierRange; i < modifierAmount; i++) {
     modifierRange = ceil(random() * 15)
-	modifierArr.push(() => random() * modifierRange)
-	dist.push(...Array(modifierRange).fill(0))
+    modifierArr.push(() => random() * modifierRange)
+    dist.push(...Array(modifierRange).fill(0))
 }
 for (let i = 0; i < 1000000; i++) dist[ceil(modifierArr.reduce((result, func) => result + func(), 0))]++
 for (const val in dist) console.log('*'.repeat(floor(dist[val] / floor(max(...dist) / width))))

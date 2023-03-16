94f7b2a7d89b96036c5e82e678dd1ca1f9a42778
Ben Isenstein <75191941+BenIsenstein@users.noreply.github.com>
2023-03-16 15:43:32 -0600

    chore(refactor): make code better in every way :)

--- bellCurve.js
+++ bellCurve.js
@@ -1,73 +1,9 @@
-const values = {}
-// max Count to run
-let maxCount = 1000000
+const { random, floor, ceil, max } = Math, width = process.stdout.columns - 10, modifierAmount = ceil(random() * 8 + 2), modifierArr = [], dist = [0]
 
-//
-// function to deal with randomness
-const randomNumber = () => Math.random()
-//
-// returns a function to create a random number on a dynamically assigned range
-let modifierGenerator = (range) => {
-	return () => randomNumber() * range
+for (let i = 0, modifierRange; i < modifierAmount; i++) {
+    modifierRange = ceil(random() * 15)
+	modifierArr.push(() => random() * modifierRange)
+	dist.push(...Array(modifierRange).fill(0))
 }
-
-// holds all modifier functions
-let modifierArr = []
-// total range of the added modifier ranges
-let totalRange = 0
-// amount of modifier to create
-let amountOfModifer = Math.ceil(randomNumber() * 8 + 2)
-// generate all modifier functions
-for (let i = 0; i < amountOfModifer; i++) {
-	let num = Math.ceil(randomNumber() * 15)
-	totalRange += num
-	modifierArr.push(modifierGenerator(num))
-	console.log(`range for generator ${i}: ${num}`)
-}
-// loading values with all needed keys
-for (let i = 0; i < totalRange + 1; i++) {
-	values[i] = 0
-}
-
-// generate a number based on all modifiers
-// log that values in values object
-function logModifier() {
-	let num = 0
-	modifierArr.every(elem => num += elem())
-	num = Math.ceil(num)
-	values[num]++
-}
-
-let count = 0
-
-// iterate up to maxCount times
-while (count < maxCount) {
-	logModifier()
-	count++
-}
-
-console.log("values:", values)
-
-// recursive function to generate many *
-function convertToDot(num) {
-	if (num == 0) return ""
-	return convertToDot(num - 1) + "*"
-}
-
-// Find max amount
-let maxVal = 0
-for (prop in values) {
-	if (values[prop] > maxVal) maxVal = values[prop]
-}
-// create increment for max amount of * to display
-let width = process.stdout.columns - 10
-let increment = Math.floor(maxVal / width)
-
-// logging our bell curve
-for (prop in values) {
-	if (values[prop] < 0) continue
-	values[prop] = Math.floor(values[prop] / increment)
-	console.log(`${prop}: ${prop < 10 ? " " : ""}${convertToDot(values[prop])}`)
-}
-
-
+for (let i = 0; i < 1000000; i++) dist[ceil(modifierArr.reduce((result, func) => result + func(), 0))]++
+for (const val in dist) console.log('*'.repeat(floor(dist[val] / floor(max(...dist) / width))))
